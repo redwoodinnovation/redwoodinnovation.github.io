44b075de9d580531daabfb705195f4762960d367
iosemio <103848482+iosemio@users.noreply.github.com>
2022-11-22 13:21:45 -0400

    Revise highways.py

--- highways.py
+++ highways.py
@@ -8,17 +8,6 @@ res = api.get("""
   nwr(37.8885,-122.2966,37.8906,-122.2945)->.all;
   (
     way.all[highway];
-    nwr.all[leisure=swimming_pool];
-    nwr.all[leisure=track];
-    nwr.all[leisure=pitch];
-    nwr.all[leisure=sports_center];
-    nwr.all[service=driveway];
-    nwr.all[man_made=flagpole];
-    nwr.all["garden:type"="community"];
-    nwr.all["garden:type"="school"];
-    nwr.all[leisure=garden];
-    nwr.all[landuse=allotments];
-    nwr.all[barrier=gate];  
   ); 
 """, verbosity='geom')
 
