b8d53023ba432b09954d41f9ba311be20c4388cd
iosemio <103848482+iosemio@users.noreply.github.com>
2022-11-28 09:52:28 -0500

    Add natural.py & geojson

--- natural.py
+++ natural.py
@@ -5,10 +5,10 @@ api = overpass.API(timeout=500)
 
 # api.get already returns a FeatureCollection
 res = api.get("""
-    (node["natural"="tree"](37.8883006925662,-122.29650825262071,37.890997478019,-122.2946360707283);
+    (nwr["natural"](37.8883006925662,-122.29650825262071,37.890997478019,-122.2946360707283);
     );
 """, verbosity='geom')
 
 # dump as a geojson file
-with open("albany/cornell/geojson/trees.geojson",mode="w") as f:
+with open("albany/cornell/geojson/natural.geojson",mode="w") as f:
   geojson.dump(res,f)
