c43e2c675844c52fc622f2e3bbc60a3145920bf1
iosemio <103848482+iosemio@users.noreply.github.com>
2022-11-30 13:39:46 -0500

    Revise Cornell

--- natural.py
+++ natural.py
@@ -6,7 +6,7 @@ api = overpass.API(timeout=500)
 # api.get returns a FeatureCollection
 res = api.get("""
     area(id:3602999142);
-    nwr["natural"]["natural"!~"tree"](area);
+    nwr["natural"]["natural"!~"tree"]["natural"!~"bay"]["natural"!~"coastline"](area);
 """, verbosity='geom')
 
 # dump as a geojson file
