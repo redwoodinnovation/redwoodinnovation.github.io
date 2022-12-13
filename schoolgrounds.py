e8ed3bb6f5b93057c63d9fb1e65dddd7f358e52e
iosemio <103848482+iosemio@users.noreply.github.com>
2022-12-13 08:17:06 -0400

    Clean up files

--- schoolgrounds.py
+++ schoolgrounds.py
@@ -1,15 +1,13 @@
 import overpass
 import geojson
 
-api = overpass.API(timeout=500)
+api = overpass.API(timeout=1500)
 
 # api.get already returns a FeatureCollection
 res = api.get("""
     area(id:3600396499);
-    (way["amenity"="school"](area);
-  relation["amenity"="school"](area);
-    );
-""", verbosity='geom')
+    nwr["amenity"="school"](area);
+ """, verbosity='geom')
 
 # dump as a geojson file
 with open("geojson/schoolgrounds.geojson",mode="w") as f:
